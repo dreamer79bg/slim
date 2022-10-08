0747eb4acb856a603a32e937575d8de0b0899095
Atanas Markov <dreamer79bg@gmail.com>
2022-10-08 18:04:05 +0300

    routing api group
    create a special routing group for apis

--- app/routes.php
+++ app/routes.php
@@ -18,6 +18,8 @@ return function (App $app) {
     $app->get('/hellotest', \App\Application\Actions\HelloWorldAction::class);
     $app->get('/twigtest', \App\Application\Actions\TwigTestAction::class);
     
-    $app->get('/api/test', \App\Application\API\TestDataAction::class);
-    $app->get('/api/testerror', \App\Application\API\TestErrorAction::class);
+    $app->group('/api', function (Group $group) {
+        $group->get('/test', \App\Application\API\TestDataAction::class);
+        $group->get('/testerror', \App\Application\API\TestErrorAction::class);
+    });
 };

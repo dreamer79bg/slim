097bb7e50e4bf9991144c98ae1a82e86900aa6e1
Atanas Markov <dreamer79bg@gmail.com>
2022-10-08 16:04:12 +0300

    setup scripts
    1. setup hooked to composer install and update
    2. execution of database migrations
    3. migrations execution log to prevent execution of same migration twice

---
+++ setup/migrations/0000000000000_Initial.sql
@@ -0,0 +1,1 @@
+create table if not exists users (id int AUTO_INCREMENT, username varchar(50), name varchar(100), password varchar(200), PRIMARY KEY (`id`));
---
+++ setup/setup.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Setup;
+
+use App\CoreComponents\MySQLDatabase;
+
+class Setup {
+    /*
+     * execute setup database migrations
+     */
+    static function doSetup() {
+        $setupPath= realpath(__DIR__);
+        $migrationsPath= $setupPath.'/migrations';
+        
+        $database= new MySQLDatabase();
+        
+        if (!file_exists($migrationsPath)) {
+            mkdir($migrationsPath);
+        }
+        
+        /*
+         * Create migrations log if not present
+         */
+        $sql= 'create table if not exists migrationslog (id int AUTO_INCREMENT,name varchar(200),dateexec DATETIME, PRIMARY KEY (`id`))';
+        $database->execute($sql);
+        
+        /*
+         * Get migrations already executed
+         */
+        $executedMigrations= array();
+        $sql= 'select * from migrationslog';
+        $res= $database->query($sql);
+        
+        foreach ($res as $val) {
+            $executedMigrations[$val['name']]= 1;
+        }
+        
+        $migrations= array();
+        $dir= opendir($migrationsPath);
+        while($fileName= readdir($dir)) {
+            if ($fileName!='.'&&$fileName!='..'&& preg_match('~\.sql$~', $fileName)) {
+                //add only if not executed
+                if (!isset($executedMigrations[$fileName])) {
+                    $migrations[]=$fileName;
+                }
+            }
+        }
+        closedir($dir);
+        
+        //sort so that execution is in proper order
+        asort($migrations,SORT_STRING);
+        
+        /*
+         * Execute migrations
+         */
+        reset($migrations);
+        foreach ($migrations as $migFile) {
+            $migText= file_get_contents($migrationsPath.'/'.$migFile)."\n\n";
+            $arrMig= explode(';',$migText);
+            reset($arrMig);
+            foreach ($arrMig as $sql) {
+                $sql= trim($sql);
+                if (!empty($sql)) {
+                    $database->execute($sql);
+                }
+            }
+    
+            //store in log
+            $sql= sprintf('insert into migrationslog (name,dateexec) values (%1$s,now())',
+                    $database->fullEscape($migFile)
+                    );
+            $database->execute($sql);
+            
+            print "\nExecuted migration $migFile";
+        }
+        
+        print "\n";
+    }
+}
+

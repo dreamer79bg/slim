b418e88d970058119a9f3a691aae91768eb530ed
Atanas Markov <dreamer79bg@gmail.com>
2022-10-23 20:09:00 +0300

    own not allowed and not found page

--- app/controllers.php
+++ app/controllers.php
@@ -6,33 +6,48 @@ return function (ContainerInterface $container) {
     $container['SecurityController'] = function ($c) {
         return new \App\Application\Controllers\SecurityController($c);
     };
-    
+
     $container['APIPostsController'] = function ($c) {
         return new \App\Application\Controllers\APIPostsController($c);
     };
-    
+
     $container['APIUsersController'] = function ($c) {
         return new \App\Application\Controllers\APIUsersController($c);
     };
-    
+
     $container['APITestsController'] = function ($c) {
         return new \App\Application\Controllers\APITestsController($c);
     };
-    
-    
+
     $container['IndexController'] = function ($c) {
         return new \App\Application\Controllers\IndexController($c);
     };
-    
+
     $container['AdminController'] = function ($c) {
         return new \App\Application\Controllers\AdminController($c);
     };
-    
+
     $container['AdminDialogsController'] = function ($c) {
         return new \App\Application\Controllers\AdminDialogsController($c);
     };
-    
+
     $container['TestsController'] = function ($c) {
         return new \App\Application\Controllers\TestsController($c);
+    };
+
+    //Override the default Not Found Handler
+    $container['notFoundHandler'] = function ($c) {
+        return function ($request, $response) use ($c) {
+            return $c['view']->render($response->withStatus(404), '404.html', [
+            ]);
+        };
+    };
+    
+    //Override the default Not Allowed Handler
+    $container['notAllowedHandler'] = function ($c) {
+        return function ($request, $response) use ($c) {
+            return $c['view']->render($response->withStatus(404), '404.html', [
+            ]);
+        };
     };
 };
